eb491c31128ec2fbcb7096c8c938793722484b22
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-04 21:58:12 +0530

    Added: Find quotient and remainder for given divisor and divider

--- QuotientRemainder.java
+++ QuotientRemainder.java
@@ -3,12 +3,12 @@ package com.bridgelab.day5.functional;
 import java.util.Scanner;
 
 public class QuotientRemainder {
-public static void calculate(int number,int divider) {
-	int N = number;
-	int D =divider;
-	int R =N%D;
-	int Q=(N-R)/D;
-	System.out.println("Quoient is : "+Q+ "  Remainder is : " +R);
+public static void calculate(int num,int div) {
+	int number = num;
+	int divider =div;
+	int remainder =number%divider;
+	int quetient=(number-remainder)/divider;
+	System.out.println("Quoient is : "+quetient+ "  Remainder is : " +remainder);
 }
 public static void main(String[] args) {
 	Scanner sc =new Scanner(System.in);
