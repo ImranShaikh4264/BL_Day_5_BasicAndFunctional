1a1a1709b558034b028439f6d9a9fa26027e8b3c
ImranShaikh4264 <shaikhimrana56@gmail.com>
2022-11-05 16:02:38 +0530

    Added:Find ditance of point (cordinate x and y) from origin

--- Distance.java
+++ Distance.java
@@ -3,16 +3,22 @@ package com.bridgelab.day5.functional;
 import java.util.Scanner;
 
 public class Distance {
+	public double findDistance(int x, int y) {
+		double distance = Math.sqrt((x * x) + (y * y));
+		return distance;
+
+	}
+
 	public static void main(String[] args) {
-		Scanner sc =new Scanner(System.in);
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter value of x");
-		int x = sc.nextInt();
+		int X = sc.nextInt();
 		System.out.println("Enter value of y");
-		int y = sc.nextInt();
-		double distance = Math.sqrt((x*x)+(y*y));
-		System.out.println("Distance is : " +distance);
-		
-				
+		int Y = sc.nextInt();
+		Distance obj = new Distance();
+		double distance = obj.findDistance(X, Y);
+		System.out.println("Distance is of point from origin : " + distance);
+
 	}
 
 }
